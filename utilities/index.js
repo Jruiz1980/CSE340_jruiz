072ee386b1c5442795d1966c15b15350854369b2
= <johnathanruiz@gmail.com>
2023-10-13 02:46:41 -0500

    add functional changes

--- utilities/index.js
+++ utilities/index.js
@@ -24,7 +24,6 @@ Util.getNav = async function (req, res, next) {
   return list;
 };
 
-module.exports = Util;
 
 
 /* **************************************
@@ -59,3 +58,5 @@ Util.buildClassificationGrid = async function(data){
   }
   return grid
 }
+
+module.exports = Util;
