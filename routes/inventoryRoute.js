3168dc422b421b22056cbf0aceef9978e918b2d1
= <johnathanruiz@gmail.com>
2023-10-13 01:34:03 -0500

    add changes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,9 +1,9 @@
 // Needed Resources
-const express = require("express");
-const router = new express.Router();
-const invController = require("../controllers/invController");
+import { Router } from "express";
+const router = new Router();
+import { buildByClassificationId } from "../controllers/invController";
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", buildByClassificationId);
 
-module.exports = router;
+export default router;
