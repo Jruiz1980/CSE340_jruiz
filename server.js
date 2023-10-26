6e9e3085d61d321bdb25c3da3277765cedd44981
= <johnathanruiz@gmail.com>
2023-10-25 23:48:34 -0500

    add changes in .env with Node version

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(async (req, res, next) => {
 *********************************** */
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  //console.error(`Error at: "${req.originalUrl}": ${err.message}`)
   if(err.status == 404) {
     message = err.message,
     image = '<img src="/images/site/error.png" alt="Error image">'
