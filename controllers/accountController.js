375178bda3bcb0d05495d5f9580bfcda41f7df8d
= <johnathanruiz@gmail.com>
2023-11-07 06:34:13 -0500

    add utilities.getNav() in accountController

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,27 +1,27 @@
-const utilities = require("../utilities")
-const accountModel = require("../models/account-model")
+const utilities = require("../utilities");
+const accountModel = require("../models/account-model");
 
-/* ****************************************
-*  Deliver login view
-* *************************************** */
-async function buildLogin(req, res, next) {
-  let nav = await utilities.getNav()
+const accountController = {};
+/* *******************
+ *  Deliver login view
+ * ******************** */
+accountController.buildLogin = async function (req, res, next) {
+  let nav = await utilities.getNav();
   res.render("account/login", {
     title: "Login",
     nav,
-  })
-}
+  });
+};
 
-/* ****************************************
-*  Deliver register view
-* *************************************** */
-async function buildRegister(req, res, next) {
-  let nav = await accountModel.getNav()
+/* **********************
+ *  Deliver register view
+ * ********************* */
+accountController.buildRegister = async function (req, res, next) {
+  let nav = await utilities.getNav();//To activate getNav() I must write utilities
   res.render("account/register", {
     title: "Register",
     nav,
-  })
-}
+  });
+};
 
-module.exports = { buildLogin, buildRegister }
-
+module.exports = accountController;
