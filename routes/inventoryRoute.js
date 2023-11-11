d5638f9eeef5d01e299141a65c48dfe15dfcc825
= <johnathanruiz@gmail.com>
2023-11-11 13:53:54 -0500

    add inventory-validator

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const invValidate = require("../utilities/inventory-validation");
 const utilities = require("../utilities")
 
 // Route to build inventory by classification view
@@ -13,5 +14,37 @@ router.get(
   utilities.handleErrors(invController.buildDetailViewById)
 );
 
+// Process the add-classification attempt
+router.post(
+  "/add-classification",
+  invValidate.addClassificationRules(),
+  invValidate.checkClassData,
+  utilities.handleErrors(invController.addClassification)
+);
 
-module.exports = router
+// Route to build the add new vehicle view
+router.get(
+  "/add-vehicle",
+  utilities.handleErrors(invController.renderAddVehicleView)
+);
+
+
+// Process adding new vehicle
+router.post(
+  "/add-vehicle",
+  invValidate.addVehicleRules(),
+  invValidate.checkVehicleData,
+  utilities.handleErrors(invController.addVehicle)
+);
+
+// New route for rendering the 'add-vehicle' view with the classification dropdown
+router.get('/add-vehicle', async (req, res) => {
+  const selectedClassificationId = req.query.selectedClassificationId;
+  const dropdown = await Util.getClassificationDropdown(req, res, null, selectedClassificationId);
+  res.render('add-vehicle', { dropdown });
+});
+
+// Add a catch-all route for 404 errors
+router.use(utilities.handleErrors);
+
+module.exports = router;
---
+++ utilities/inventory-validation.js
@@ -0,0 +1,185 @@
+const uitilities = require(".");
+const { body, validationResult } = require("express-validator");
+const classificationModel = require("../models/inventory-model");
+const inv_validate = {};
+
+/* ******************************
+ * Check classification data and return errors
+ * or continue to the management view
+ * ***************************** */
+inv_validate.addClassificationRules = () => {
+  return [
+    body("classification_name")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Please provide a classification name.")
+      .custom(async (classification_name) => {
+        const classificationExists =
+          await classificationModel.checkExistingClassification(
+            classification_name
+          );
+
+        if (classificationExists) {
+          console.log("Classification already exists");
+          throw new Error("Classification name already exists.");
+        }
+
+        // Custom validation to check for spaces or special characters
+        if (
+          /\s/.test(classification_name) ||
+          /[!@#$%^&*(),.?":{}|<>]/.test(classification_name)
+        ) {
+          throw new Error(
+            "Classification name cannot contain spaces or special characters."
+          );
+        }
+      }),
+  ];
+};
+
+/* ******************************
+ * Check data and return errors or continue to Classification
+ * ***************************** */
+inv_validate.checkClassData = async (req, res, next) => {
+  let nav = await utilities.getNav();
+  const { classification_name } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // Handle validation errors, render a form with error messages
+    res.render("inventory/add-classification", {
+      errors,
+      title: "Add New Classification",
+      nav,
+      classification_name,
+    });
+    return;
+  }
+  next();
+};
+
+/*  **********************************
+ *  New Vehicle Data Validation Rules
+ * ********************************* */
+inv_validate.addVehicleRules = () => {
+  return [
+    // Make is required and must contain 3 characters minimum
+    body("inv_make")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage(
+        "Please provide a vehicle make with a minimum of 3 characters."
+      ),
+
+    // Model is required and must contain 3 characters minimum
+    body("inv_model")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage(
+        "Please provide a vehicle model with a minimum of 3 characters."
+      ),
+
+    // Description is required
+    body("inv_description")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Please provide a vehicle description."),
+
+    // Image path is required
+    body("inv_image")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Please provide an image path."),
+
+    // Thumbnail path is required
+    body("inv_thumbnail")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Please provide a thumbnail path."),
+
+    // Price is required and must be numeric
+    body("inv_price")
+      .trim()
+      .custom((value) => {
+        // Check if the value is numeric with an optional decimal
+        if (!/^\d+(\.\d{1,2})?$/.test(value)) {
+          throw new Error(
+            "Please provide a valid price with up to two decimal places."
+          );
+        }
+        return true;
+      })
+      .withMessage(
+        "Please provide a valid price with up to two decimal places."
+      ),
+
+    // Year is required and must be 4 digits
+    body("inv_year")
+      .trim()
+      .isNumeric()
+      .custom((value) => {
+        if (value.length !== 4 || !/^\d+$/.test(value)) {
+          throw new Error("Please provide a 4 digit year.");
+        }
+        return true;
+      })
+      .withMessage("Please provide a 4 digit year."),
+
+    // Miles is required and must be numeric
+    body("inv_miles")
+      .trim()
+      .custom((value) => {
+        if (!/^\d+$/.test(value)) {
+          throw new Error("Please provide a valid numerical mileage.");
+        }
+        return true;
+      })
+      .withMessage("Please provide a valid numerical mileage."),
+
+    // Color is required
+    body("inv_color")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Please provide a color."),
+  ];
+};
+
+/* ******************************
+ * Check data and return errors or continue to Management View
+ * ***************************** */
+inv_validate.checkVehicleData = async (req, res, next) => {
+  const {
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+  } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav();
+    let classificationDropdown = await Util.getClassificationDropdown();
+    res.render("inventory/add-vehicle", {
+      errors,
+      title: "Add New Vehicle",
+      nav,
+      classificationDropdown,
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+    });
+    return;
+  }
+  next();
+};
+
+module.exports = inv_validate;
