8222952b11d20b3a3f63e7cea647c5e6e1c640f3
= <johnathanruiz@gmail.com>
2023-11-11 13:21:02 -0500

    add crypt

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -45,13 +45,13 @@ accountController.registerAccount = async function (req, res) {
   let hashedPassword;
   try {
     // regular password and cost (salt is generated automatically)
-    hashedPassword = await bcrypt.hashSync(account_password, 10);
+    hashedPassword = bcrypt.hashSync(account_password, 10);
   } catch (error) {
     req.flash(
       "notice",
       "Sorry, there was an error processing the registration."
     );
-    res.status(500).render("account/register", {
+    return res.status(500).render("account/register", {
       title: "Registration",
       nav,
       errors: null,
@@ -62,7 +62,7 @@ accountController.registerAccount = async function (req, res) {
     account_firstname,
     account_lastname,
     account_email,
-    account_password,
+    //account_password,
     hashedPassword
   );
 
@@ -71,13 +71,13 @@ accountController.registerAccount = async function (req, res) {
       "notice",
       `Congratulations, you\'re registered ${account_firstname}. Please log in.`
     );
-    res.status(201).render("account/login", {
+    return res.status(201).render("account/login", {
       title: "Login",
       nav,
     });
   } else {
     req.flash("notice", "Sorry, the registration failed.");
-    res.status(501).render("account/register", {
+    return res.status(501).render("account/register", {
       title: "Registration",
       nav,
     });
